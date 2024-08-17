4e4316efd8aa69abd1aa82cb5a921d0f7a7c4435
DeLo <gustavohm102@gmail.com>
2024-08-16 23:06:42 -0300

    first playable state

--- player/player.gd
+++ player/player.gd
@@ -1,22 +1,44 @@
 extends Area2D
 
+# x: size , y: look_dir, z: pose_frame
+var player_state := Vector3(1, 0, 0)
+
+func _ready() -> void:
+	SignalBus.wall_hit.connect(on_wall_hit)
+
 func _unhandled_input(_event: InputEvent) -> void:
-	var size := 1 + (0.5 * Input.get_axis("size_down", "size_up"))
-	scale = Vector2(size, size)
+	player_state.x = Input.get_axis("size_down", "size_up")
+	player_state.y = Input.get_axis("look_left", "look_right")
+	player_state.z = 0
 	
-	var look_dir := Input.get_axis("look_left", "look_right")
-	if look_dir != 0:
-		$Sprite2D/Line2D.rotation = 0
-		scale.x *= look_dir
+	if player_state.y != 0:
+		if Input.is_action_pressed("pose_down"):
+			player_state.z = 1
+		if Input.is_action_pressed("pose_up"):
+			player_state.z = 2
+		if Input.is_action_pressed("pose_left"):
+			player_state.z = 3
+		if Input.is_action_pressed("pose_right"):
+			player_state.z = 4
+	
+	scale = Vector2(1 + (0.5 * player_state.x), 1 + (0.5 * player_state.x))
+	$Sprite2D.flip_h = (player_state.y == -1) 
+	$Sprite2D.frame = player_state.z
+
+func on_wall_hit(wall_pose : Vector3) -> void:
+	if wall_pose.x == player_state.x && wall_pose.y == player_state.y && wall_to_player(wall_pose.z) == player_state.z:
+		print("Nice fit")
 	else:
-		$Sprite2D/Line2D.rotation_degrees = -90
-	
-	$Sprite2D.frame = 0
-	if Input.is_action_pressed("pose_down"):
-		$Sprite2D.frame = 1
-	if Input.is_action_pressed("pose_up"):
-		$Sprite2D.frame = 2
-	if Input.is_action_pressed("pose_left"):
-		$Sprite2D.frame = 3
-	if Input.is_action_pressed("pose_right"):
-		$Sprite2D.frame = 4
+		print("Ouch")
+
+func wall_to_player(wall_pose_num : int) -> int:
+	match wall_pose_num:
+		1:
+			return 2
+		2:
+			return 3
+		3:
+			return 4
+		4:
+			return 1
+	return 0
