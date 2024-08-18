55448f416199cb2ad837476829dadcf226389140
Omar - Home <negmok@gmail.com>
2024-08-17 19:50:57 -0500

    Change ordering when player passes through wall

--- levels/level.gd
+++ levels/level.gd
@@ -4,12 +4,12 @@ extends Node2D
 @onready var wall_spawner = WallSpawner.new()
 
 func _ready():
-	$WallSpawnPoint.add_child(wall_spawner)
 	var song = SongSequence.new(1)
 	add_child(song)
+	add_child(wall_spawner)
 	song.timestamp_reached.connect(on_timestamp_reached)
 	await get_tree().create_timer(3).timeout
 	song.play()
 
 func on_timestamp_reached():
-	wall_spawner.spawn_wall()
+	wall_spawner.spawn_wall($WallSpawnPoint.global_position)
