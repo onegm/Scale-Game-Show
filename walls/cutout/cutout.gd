786e74637bc1a69a95819d426146fde14bde7fb2
Omar - Home <negmok@gmail.com>
2024-08-19 10:39:06 -0500

    update wall breaking

--- walls/cutout/cutout.gd
+++ walls/cutout/cutout.gd
@@ -1,8 +1,25 @@
-extends AnimatedSprite2D
+extends Node2D
+
+@onready var clipping_sprite := $ClippingSprite
+@onready var cover_sprite := $CoverSprite
+var wall
+var wall_cover
 
 func set_posture(index : int, flipped : bool):
-	flip_h = flipped
-	play(str(index))
+	clipping_sprite.flip_h = flipped
+	clipping_sprite.play(str(index))
+	cover_sprite.flip_h = flipped
+	cover_sprite.play(str(index))
 
 func set_posture_scale(new_scale : float):
 	scale = new_scale*Vector2.ONE
+	wall.scale /= new_scale
+	wall_cover.scale /= new_scale
+	
+func set_wall(new_wall : Node2D):
+	wall = new_wall
+	wall.reparent(clipping_sprite)
+
+func set_cover(new_wall_cover : Node2D):
+	wall_cover = new_wall_cover
+	wall_cover.reparent(cover_sprite)
--- walls/wall.gd
+++ walls/wall.gd
@@ -1,9 +1,6 @@
 extends Area2D
 
 @onready var cutout = $Cutout
-@onready var wall = $Cutout/Wall
-@onready var cutout_cover = $CutoutCover
-@onready var wall_cover = $CutoutCover/WallCover
 @onready var outlines = $Outlines
 @onready var current_wall_detector := $CurrentWallDetector
 
@@ -17,30 +14,29 @@ func _ready():
 	current_wall_detector.area_entered.connect(func(_area): is_current_wall=true)
 	current_wall_detector.area_exited.connect(func(_area): is_current_wall=false)
 	
+	cutout.set_wall($Wall)
+	cutout.set_cover($WallCover)
+	
 func on_area_exited(_area):
 	SignalBus.wall_hit.emit(postureDTO)
 
 func _physics_process(delta):
 	global_position.x -= speed*delta
-	if global_position.x < 0:
+	if global_position.x < -200:
 		queue_free()
 
 func set_posture_from_DTO(new_posture : PostureDTO):
-	assert(new_posture.posture in PostureDTO.wall_postures, "Invalid wall posture choice: " + str(new_posture.posture))
+	assert(new_posture.posture in PostureDTO.wall_postures, "Invalid wall posture choice: " + PostureDTO.POSTURE.keys()[new_posture.posture])
 	postureDTO = new_posture
-	set_wall_and_posture_scale(postureDTO.size)
+	set_cutout_and_outline_scale(postureDTO.size)
 	cutout.set_posture(postureDTO.posture, postureDTO.direction == postureDTO.DIRECTION.RIGHT)
-	cutout_cover.set_posture(postureDTO.posture, postureDTO.direction == postureDTO.DIRECTION.RIGHT)
 	
 	var posture_num = (postureDTO.posture + PostureDTO.wall_postures.size()) if postureDTO.direction == postureDTO.DIRECTION.RIGHT else postureDTO.posture
 	outlines.play(str(posture_num))
 	
-func set_wall_and_posture_scale(new_scale : float):
+func set_cutout_and_outline_scale(new_scale : float):
 	new_scale = (new_scale + 2.0)/1.5
 	cutout.set_posture_scale(new_scale)
-	cutout_cover.set_posture_scale(new_scale)
-	wall.scale /= new_scale
-	wall_cover.scale /= new_scale
 	outlines.scale = Vector2.ONE*new_scale
 
 func on_player_missed():
@@ -51,8 +47,8 @@ func break_wall():
 	$WallPieces.visible = true
 	outlines.visible = false
 	cutout.visible = false
-	cutout_cover.visible = false
-	var tween = get_tree().create_tween().set_parallel(true)
+	var tween = get_tree().create_tween().set_parallel(true).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
 	tween.tween_property($WallPieces/LeftWall, "rotation", -2, 1)
 	tween.tween_property($WallPieces/RightWall, "rotation", 2, 1)
-	tween.chain().tween_callback($WallPieces.queue_free)
+	tween.tween_property($WallPieces/RightWall, "position:x", 400, 1)
+	tween.tween_property($WallPieces, "position:y", 700, 1).set_ease(Tween.EASE_IN)
